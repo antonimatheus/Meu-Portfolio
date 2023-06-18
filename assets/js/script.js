ba7d4ae2e932224f4b949e03d7d5208f7c16d0bd
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-06-18 15:15:47 -0400

    Media Query criado

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const navEl = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY >= 56) {
-    navEl.classList.add('navbar-scrolled')
+    navEl.classList.add("navbar-scrolled")
   } else if (window.scrollY < 56) {
     navEl.classList.remove('navbar-scrolled')
   }

09dda4e5b1818752cc2f71b6f53e515eb7960dd3
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-06-19 11:45:10 -0400

    responsive mobile finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-const navEl = document.querySelector('.navbar');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY >= 56) {
-    navEl.classList.add("navbar-scrolled")
-  } else if (window.scrollY < 56) {
-    navEl.classList.remove('navbar-scrolled')
-  }
-})
 
 
 const slider = document.querySelector('#iframe-container iframe');

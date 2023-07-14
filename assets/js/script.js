6ea36c7336a9bab5b91385ab92af71d1314fb232
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-07-14 19:12:20 -0400

    update v7

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,30 +12,3 @@ const observer = new IntersectionObserver((entries) => {
 })
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el))
-
-
-const slider = document.querySelector('#iframe-container iframe');
-let isDown = false;
-let startX;
-let scrollLeft;
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; // Defina a sensibilidade do arrasto alterando o multiplicador
-  slider.scrollLeft = scrollLeft - walk;
-});

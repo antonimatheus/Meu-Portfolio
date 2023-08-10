1729c91ca50fba935e74210c698d5273786b3c7b
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-08-10 11:06:25 -0400

    update language to one single page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+const b1 = document.getElementById("body1")
+const b2 = document.getElementById("body2")
+const br = document.getElementById("br")
+const us = document.getElementById("us")
+
+br.addEventListener("click", function() {
+  b1.style.display = 'none'
+  b2.style.display = 'block'
+})
+
+us.addEventListener("click", function() {
+  b1.style.display = 'block'
+  b2.style.display = 'none'
+})
+
+
+
+
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry)

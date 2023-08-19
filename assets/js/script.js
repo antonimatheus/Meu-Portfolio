8f7f7b49a8a4d9588023788f7afdf117e00309d1
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-08-19 17:57:21 -0400

    updating the projects main

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,60 @@ const b2 = document.getElementById("body2")
 const br = document.getElementById("br")
 const us = document.getElementById("us")
 
+const imgBlock1 = document.getElementById("imgBlock1");
+const imgBlock2 = document.getElementById("imgBlock2");
+const imgBlock3 = document.getElementById("imgBlock3");
+const imgBlock4 = document.getElementById("imgBlock4");
+
+const desk1 = document.getElementById("desk1");
+const mob1 = document.getElementById("mob1");
+const desk2 = document.getElementById("desk2");
+const mob2 = document.getElementById("mob2");
+const desk3 = document.getElementById("desk3");
+const mob3 = document.getElementById("mob3");
+const desk4 = document.getElementById("desk4");
+//const mob4 = document.getElementById("mob4");
+
+desk1.addEventListener("click", function() {
+  imgBlock1.innerHTML = '<img src="../assets/images/sunnyside-desktop.png" alt="Desktop Image" />';
+})  
+
+mob1.addEventListener("click", function() {
+  imgBlock1.innerHTML = '<img src="../assets/images/sunnyside-mobile.png" alt="Mobile Image" />';
+});
+
+desk2.addEventListener("click", function() {
+  imgBlock2.innerHTML = '<img src="../assets/images/android-desktop.png" alt="Desktop Image" />';
+});
+
+mob2.addEventListener("click", function() {
+  imgBlock2.innerHTML = '<img src="../assets/images/android-mobile.png" alt="Mobile Image" />';
+  
+});
+
+desk3.addEventListener("click", function() {
+  imgBlock3.innerHTML = '<img src="" alt="Desktop Image" />';
+});
+
+mob3.addEventListener("click", function() {
+  imgBlock3.innerHTML = '<img src="" alt="Mobile Image" />';
+  
+});
+
+desk4.addEventListener("click", function() {
+  imgBlock4.innerHTML = '<img src="assets/images/login-light-dark-desktop.png" alt="Desktop Image" />';
+});
+
+//mob4.addEventListener("click", function() {
+//  imgBlock4.innerHTML = '<img src="" alt="Mobile Image" />';
+//});
+
+
+
+
+
+
+
 br.addEventListener("click", function() {
   b1.style.display = 'none'
   b2.style.display = 'block'

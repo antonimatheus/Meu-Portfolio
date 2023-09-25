7caa525ff5c49e1b8b0950987a828d1fe29e3583
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-09-25 11:52:19 -0400

    update v10

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,115 +1,64 @@
-const b1 = document.getElementById("body1")
-const b2 = document.getElementById("body2")
-const br = document.getElementById("br")
-const us = document.getElementById("us")
+const sunny1 = document.getElementById("sunny1");
+const sunny2 = document.getElementById("sunny2");
 
-br.addEventListener("click", function() {
-  b1.style.display = 'none'
-  b2.style.display = 'block'
-})
+const android1 = document.getElementById("android1");
+const android2 = document.getElementById("android2");
 
-us.addEventListener("click", function() {
-  b1.style.display = 'block'
-  b2.style.display = 'none'
-})
+const interactive1 = document.getElementById("interactive1");
+const interactive2 = document.getElementById("interactive2");
 
-
-const imgBlock1 = document.getElementById("imgBlock1");
-const imgBlock2 = document.getElementById("imgBlock2");
-const imgBlock3 = document.getElementById("imgBlock3");
-const imgBlock4 = document.getElementById("imgBlock4");
-const imgBlock12 = document.getElementById("imgBlock12");
-const imgBlock22 = document.getElementById("imgBlock22");
-const imgBlock32 = document.getElementById("imgBlock32");
-const imgBlock42 = document.getElementById("imgBlock42");
+const login1= document.getElementById("login1");
 
 const desk1 = document.getElementById("desk1");
 const mob1 = document.getElementById("mob1");
+
 const desk2 = document.getElementById("desk2");
 const mob2 = document.getElementById("mob2");
+
 const desk3 = document.getElementById("desk3");
 const mob3 = document.getElementById("mob3");
+
 const desk4 = document.getElementById("desk4");
 //const mob4 = document.getElementById("mob4");
 
-const desk12 = document.getElementById("desk12");
-const mob12 = document.getElementById("mob12");
-const desk22 = document.getElementById("desk22");
-const mob22 = document.getElementById("mob22");
-const desk32 = document.getElementById("desk32");
-const mob32 = document.getElementById("mob32");
-const desk42 = document.getElementById("desk42");
-//const mob4 = document.getElementById("mob4");
-
 desk1.addEventListener("click", function() {
-  imgBlock1.innerHTML = '<img src="assets/images/sunnyside-desktop.png" alt="Desktop Image" />';
+  sunny1.style.display = 'flex'
+  sunny2.style.display = 'none'
 })  
 
 mob1.addEventListener("click", function() {
-  imgBlock1.innerHTML = '<img src="assets/images/sunnyside-mobile.png" alt="Mobile Image" />';
+  sunny1.style.display = 'none'
+  sunny2.style.display = 'flex'
 });
 
 desk2.addEventListener("click", function() {
-  imgBlock2.innerHTML = '<img src="assets/images/android-desktop.png" alt="Desktop Image" />';
+  android1.style.display = 'flex'
+  android2.style.display = 'none'
 });
 
 mob2.addEventListener("click", function() {
-  imgBlock2.innerHTML = '<img src="assets/images/android-mobile.png" alt="Mobile Image" />';
+  android1.style.display = 'none'
+  android2.style.display = 'flex'
 });
 
 desk3.addEventListener("click", function() {
-  imgBlock3.innerHTML = '<img src="assets/images/interactive-component-desktop.png" alt="Desktop Image" />';
+  interactive1.style.display = 'flex' 
+  interactive2.style.display = 'none' 
 });
 
 mob3.addEventListener("click", function() {
-  imgBlock3.innerHTML = '<img src="assets/images/interactive-component-mobile.png" alt="Mobile Image" />';
+  interactive1.style.display = 'none' 
+  interactive2.style.display = 'flex' 
   
 });
 
 desk4.addEventListener("click", function() {
-  imgBlock4.innerHTML = '<img src="assets/images/login-light-dark-desktop.png" alt="Desktop Image" />';
+  login1.style.display = 'flex'
 });
 
 //mob4.addEventListener("click", function() {
 //  imgBlock4.innerHTML = '<img src="" alt="Mobile Image" />';
 //});
-
-
-
-desk12.addEventListener("click", function() {
-  imgBlock12.innerHTML = '<img src="assets/images/sunnyside-desktop.png" alt="Desktop Image" />';
-})  
-
-mob12.addEventListener("click", function() {
-  imgBlock12.innerHTML = '<img src="assets/images/sunnyside-mobile.png" alt="Mobile Image" />';
-});
-
-desk22.addEventListener("click", function() {
-  imgBlock22.innerHTML = '<img src="assets/images/android-desktop.png" alt="Desktop Image" />';
-});
-
-mob22.addEventListener("click", function() {
-  imgBlock22.innerHTML = '<img src="assets/images/android-mobile.png" alt="Mobile Image" />';
-  
-});
-
-desk32.addEventListener("click", function() {
-  imgBlock32.innerHTML = '<img src="assets/images/interactive-component-desktop.png" alt="Desktop Image" />';
-});
-
-mob32.addEventListener("click", function() {
-  imgBlock32.innerHTML = '<img src="assets/images/interactive-component-mobile.png" alt="Mobile Image" />';
-  
-});
-
-desk42.addEventListener("click", function() {
-  imgBlock42.innerHTML = '<img src="assets/images/login-light-dark-desktop.png" alt="Desktop Image" />';
-});
-
-//mob42.addEventListener("click", function() {
-//  imgBlock42.innerHTML = '<img src="" alt="Mobile Image" />';
-//});
-
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -123,3 +72,15 @@ const observer = new IntersectionObserver((entries) => {
 })
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el))
+
+
+const desiredElement = document.getElementById('navbar'); // elemento alvo
+const pixelsAmount = 50; // Quantidade de pixels a contar do TOP até definir a cor
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > pixelsAmount) {
+        desiredElement.style.backgroundColor = 'black';
+    } else {
+        desiredElement.style.backgroundColor = 'transparent';
+    }
+});
